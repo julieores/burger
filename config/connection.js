9222a2bce50b36a9cd05dfd5a31cc857b933d54a
Julie Ores <julie.ores@gmail.com>
2018-10-22 11:34:17 -0500

    minor changes including ports

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,12 @@ if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 connection = mysql.createConnection({
-port: 3300,
+port: 3306,
 host: "localhost",
 user: "root",
 password: "password",
 database: "burgers_db"
-});
+})
 
 };
 
